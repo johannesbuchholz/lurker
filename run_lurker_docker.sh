0ad07eccf8e2600ee04fab980dfdd71dc63a31d6
jbuchholz <johannesbuchholz@outlook.com>
2024-09-06 18:28:22 +0200

    build: Simplify docker run convenience script

--- run_lurker_docker.sh
+++ run_lurker_docker.sh
@@ -1,21 +1,14 @@
 #!/bin/sh
 # This is a convenience script to launch the lurker docker image with appropriate options to correctly mount
 # configuration and expose sound devices of the host machine.
+# Example:
+#   sh run_lurker_docker.sh /abs/path/to/lurkerhome
 
 set -e
 
-get_first_media_device_on_host() {
-  # use -mindepth and -maxdepth to exclude the root directory
-  find "/media/$(whoami)" -maxdepth 1 -mindepth 1 -type d 2>/dev/null | head -1
-}
+input_host_lurker_home="$1"
 
-get_lurker_home_path_on_host() {
-  # only use first encountered media device if LURKER_USE_MEDIA_OF_HOST is set and not null
-  first_media_device="${LURKER_USE_MEDIA_OF_HOST:+$(get_first_media_device_on_host)}"
-  echo "${first_media_device:-${HOME}}/lurker"
-}
-
-host_lurker_home=$(get_lurker_home_path_on_host)
+host_lurker_home="${input_host_lurker_home:-${HOME}/lurker}"
 echo "Determined lurker home on host machine: ${host_lurker_home}"
 
 lurker_image="${LURKER_IMAGE:-johannesbuchholz/lurker:latest}"
