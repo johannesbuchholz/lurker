2a8cd250ddf5b07a9dba692b433f9fcb7b334720
jbuchholz <johannesbuchholz@outlook.com>
2024-09-07 20:58:29 +0200

    chore: Adjust runner script to better search in mounted media devices

--- run_lurker_docker.sh
+++ run_lurker_docker.sh
@@ -2,16 +2,21 @@
 # This is a convenience script to launch the lurker docker image with appropriate options to correctly mount
 # configuration and expose sound devices of the host machine.
 # Example:
-#   sh run_lurker_docker.sh /abs/path/to/lurkerhome
+#   export LURKER_USE_HOST_MEDIA=t && sh run_lurker_docker.sh
+
+find_lurker_home_in_media() {
+  # use -mindepth and -maxdepth to exclude the root directory
+  find "/media" -maxdepth 4 -type d -name lurker 2>/dev/null | head -1
+}
 
 set -e
 
-input_host_lurker_home="$1"
+media_lurker_home="${LURKER_USE_HOST_MEDIA:+$(find_lurker_home_in_media)}"
+host_lurker_home="${media_lurker_home:-${HOME}/lurker}"
 
-host_lurker_home="${input_host_lurker_home:-${HOME}/lurker}"
 echo "Determined lurker home on host machine: ${host_lurker_home}"
 
-lurker_image="${LURKER_IMAGE:-johannesbuchholz/lurker:latest}"
+lurker_image="${LURKER_IMAGE:-lurker:latest}"
 echo "Running lurker image: ${lurker_image}"
 docker run \
     --device /dev/snd \
