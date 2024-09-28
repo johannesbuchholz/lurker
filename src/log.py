e5608f5ad34252418e336a3134ff24ca7921e493
jbuchholz <johannesbuchholz@outlook.com>
2024-09-28 02:46:07 +0200

    feat(log): Enable option to log to file

--- src/log.py
+++ src/log.py
@@ -1,19 +1,24 @@
 import logging
+import sys
 from logging import Logger
-from typing import List
+from typing import List, Optional
 
 _LOGGERS: List[Logger] = []
 
 
-def new_logger(name: str) -> Logger:
+def new_logger(name: str, file: Optional[str] = None) -> Logger:
     logger: Logger = logging.getLogger("Lurker ({})".format(name))
 
     logger.setLevel("INFO")
     logger.propagate = False
 
-    handler = logging.StreamHandler()
-    handler.setLevel(logger.level)
+    if file is None:
+        handler = logging.StreamHandler(sys.stdout)
+    else:
+        handler = logging.FileHandler(filename=file)
+
     formatter = logging.Formatter('[%(levelname)8s] %(name)s: %(message)s')
+    handler.setLevel(logger.level)
     handler.setFormatter(formatter)
 
     # make logger only use this one handler
