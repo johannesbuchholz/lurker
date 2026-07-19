32d801653dc9594eb8e60262d46384c507948e73
Johannes Buchholz <johannesbuchholz@outlook.com>
2026-07-19 16:54:19 +0200

    wip: Remove duplicate transcription state
    Vosk models internal state is enough.

--- src/transcription.py
+++ src/transcription.py
@@ -1,6 +1,5 @@
 import json
 import threading
-from collections import deque
 from typing import Callable
 
 from vosk import Model, KaldiRecognizer
@@ -11,14 +10,13 @@ from src.keyword import Keyword
 
 class Transcriber:
 
-    def __init__(self, callback: Callable[[str], None], keyword: Keyword, model_path: str, sample_rate: int = 16000, max_words: int = 80):
+    def __init__(self, callback: Callable[[str], None], keyword: Keyword, model_path: str, sample_rate: int = 16000):
         self._logger = log.new_logger(self.__class__.__name__)
 
         self._model = Model(model_path)
         self._recognizer = KaldiRecognizer(self._model, sample_rate)
         self._recognizer.SetWords(True)
 
-        self._transcription: deque[str] = deque(maxlen=max_words)
         self._callback = callback
         self._keyword = keyword
 
@@ -30,30 +28,32 @@ class Transcriber:
         Checks accumulated transcription for keyword and fires callback if found.
         On keyword match, resets recognizer and clears transcription.
         """
-        partial_str = self._recognizer.PartialResult()
-        partial = {}
-        try:
-            partial = json.loads(partial_str)
-        except Exception as e:
-            self._logger.warning(f"Could not read partial result: {partial_str} ({e})", exc_info=True)
-        partial_text = partial.get("partial", "").strip()
-        if partial_text:
-            self._transcription.extend(partial_text.split())
+        complete_asr_result: str = self._recognizer.Result()
+        if not complete_asr_result:
+            # full result not available: us current partial result
+            partial_json = {}
+            try:
+                partial_json = json.loads(self._recognizer.PartialResult())
+            except Exception as e:
+                self._logger.warning(f"Could not read partial result: {partial_json} ({e})", exc_info=True)
+            result_to_check = partial_json.get("partial", "").strip()
+            asr_completed = False
+        else:
+            # asr thinks, this is a complete result.
+            complete_json = {}
+            try:
+                complete_json = json.loads(complete_asr_result)
+            except Exception as e:
+                self._logger.warning(f"Could not read partial result: {complete_json} ({e})", exc_info=True)
+            result_to_check = complete_json.get("text", "").strip()
+            asr_completed = True
 
-        full_text = self._get_text()
-        keyword_end_index = self._keyword.is_in(full_text)
+        keyword_end_index = self._keyword.is_in(result_to_check)
         instruction = ""
         has_keyword = keyword_end_index is not None
         if has_keyword:
-            instruction = full_text[keyword_end_index:].strip()
-            self._reset()
+            instruction = result_to_check[keyword_end_index:].strip()
+            self._recognizer.Reset()
             if instruction:
                 threading.Thread(name=instruction, target=self._callback, args=(instruction,), daemon=True).start()
-        self._logger.debug(f"Flush (keyword={has_keyword}): full_text=\"{full_text}\", instruction=\"{instruction}\"")
-
-    def _reset(self):
-        self._recognizer.Reset()
-        self._transcription.clear()
-
-    def _get_text(self) -> str:
-        return " ".join(self._transcription)
+        self._logger.debug(f"Checked: has_keyword={has_keyword}, text=\"{result_to_check}\", instruction=\"{instruction}\"")
